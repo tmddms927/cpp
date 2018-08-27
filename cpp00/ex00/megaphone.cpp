dacd97eda6faa11baddcc3cb149795dcdc378ff1
tmddms927 <tmddms927@gmail.com>
2018-08-27 20:19:19 +0900

    feat:ex00 makefile

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seung-eun <seung-eun@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/28 12:39:46 by seung-eun         #+#    #+#             */
+/*   Updated: 2021/08/28 12:55:57 by seung-eun        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return 0;
+}
