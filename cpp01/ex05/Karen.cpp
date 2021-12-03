f1cf607cc02ebbd83a4b9423fefec2ecc5c734ec
tmddms927 <tmddms927@gmail.com>
2021-12-03 11:22:45 +0900

    fix: cpp01/ex05 finished

--- cpp01/ex05/Karen.cpp
+++ cpp01/ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: seungoh <seungoh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 09:39:31 by seungoh           #+#    #+#             */
-/*   Updated: 2021/12/02 11:01:40 by seungoh          ###   ########.fr       */
+/*   Updated: 2021/12/03 11:17:38 by seungoh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 
 Karen::Karen()
 {
-	this->level[0].name = "DEBUG";
-	this->level[0].f = &Karen::debug;
-	this->level[1].name = "INFO";
-	this->level[1].f = &Karen::info;
-	this->level[2].name = "WARNING";
-	this->level[2].f = &Karen::warning;
-	this->level[3].name = "ERROR";
-	this->level[3].f = &Karen::error;
+	
 }
 
 Karen::~Karen()
@@ -31,33 +24,41 @@ Karen::~Karen()
 
 void Karen::debug(void)
 {
-	std::cout << "I love to get extra bacon for\
+	std::cout << "<DUBUG> I love to get extra bacon for\
 	my 7XL-double-cheese-triple-pickle-special-ketchup burger.\
 	I just love it!" << std::endl;
 }
 
 void Karen::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon cost more money.\
+	std::cout << "<INFO> I cannot believe adding extra bacon cost more money.\
 	You don’t put enough! If you did I would not have to ask for it!" << std::endl;	
 }
 
 void Karen::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been\
+	std::cout << "<WARNING> I think I deserve to have some extra bacon for free. I’ve been\
 	coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::error(void)
 {
-	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+	std::cout << "<ERROR> This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void Karen::complain(std::string level)
 {
+	void	(Karen::*f[])(void) = { &Karen::debug, &Karen::info, &Karen::warning, &Karen::error };
+	std::string	name[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	/*
+	** 1. 멤버 함수는 일반 함수와 지칭 방법이 조금은 다르다. 일반 함수와 달리 무조건 주소를 줘야한다 (이름을 못 줌)
+	** 2. 그래서 호출을 할 때 무조건 역참조가 필요하다.
+	** 3. 호출 시에 역참조를 하더라도 네임스페이스를 인식시킬 필요가 있다.
+	*/
+
 	for (int i = 0; i < 4; ++i)
 	{
-		if (level == this->level[i].name)
-			(this->level[i]).f();
+		if (name[i] == level)
+			(this->*(f[i]))();
 	}
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seungoh <seungoh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 11:14:50 by seungoh           #+#    #+#             */
-/*   Updated: 2021/12/02 10:27:57 by seungoh          ###   ########.fr       */
+/*   Updated: 2021/12/03 11:20:49 by seungoh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int		main(void)
 {
-	
+    Karen karen;
+
+    karen.complain("DEBUG");
+    karen.complain("INFO");
+    karen.complain("WARNING");
+    karen.complain("ERROR");
+    karen.complain("ERROR22");
+
+    return 0;
 }
