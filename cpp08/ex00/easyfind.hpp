d18e43e65adab73951e73e1ffef7d02d6f091178
오승은 <tmddms927@gmail.com>
2022-01-17 17:46:27 +0900

    feat: cpp08-ex01 clear

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+
+#ifndef EASYFIND_H
+#define EASYFIND_H
+
+template< typename T >
+typename T::iterator easyfind(T int_v, int index);
+
+#endif
