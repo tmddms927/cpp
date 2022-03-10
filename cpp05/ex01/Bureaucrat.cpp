3a94f81922e3688583411588b8b0020a33608061
오승은 <tmddms927@gmail.com>
2022-03-10 15:01:25 +0900

    feat: cpp05-ex01

---
+++ cpp05/ex01/Bureaucrat.cpp
@@ -0,0 +1,49 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(std::string _name, int _grade):name(_name), grade(_grade) {
+}
+
+std::string Bureaucrat::getName(void) {
+	return name;
+}
+
+int Bureaucrat::getGrade(void) {
+	return grade;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return "too higt exception!";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return "too low exception!";
+}
+
+void Bureaucrat::increamentGrade(void) {
+	if (grade + 1 > GRADE_MIN)
+		throw GradeTooLowException();
+}
+
+void Bureaucrat::decreamentGrade(void) {
+	if (grade - 1 < GRADE_MAX)
+		throw GradeTooHighException();
+}
+
+std::ostream& operator<<(std::ostream &out, const Bureaucrat &bureaucrat) {
+	Bureaucrat temp = bureaucrat;
+	out << "name : " << temp.getName() << ", grade : " << temp.getGrade();
+	return out;
+}
+
+////////// ex01 추가
+void Bureaucrat::signForm(Form &form) {
+	try {
+		form.beSigned(grade);
+	}
+	catch (std::exception &e){
+		std::cout << name << " cannot sign " << form.getName() <<
+			" because " << e.what() << std::endl;
+		return ;
+	}
+	std::cout << name << " signs " << form.getName() << std::endl;
+}
---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,35 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include <iostream>
+#include <exception>
+#include <string>
+
+#define GRADE_MAX 1
+#define GRADE_MIN 100
+
+class Form {
+private:
+	const std::string name;
+	bool sign;
+	const int signed_grade;
+	const int execute_grade;
+
+public:
+	Form(std::string _name, int _signed_grade, int _excute_grade);
+	class GradeTooHighException : public std::exception {
+		const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+		const char* what() const throw();
+	};
+	std::string getName(void);
+	bool getSign(void);
+	int getSignedGrade(void);
+	int getExecuteGrade(void);
+	void beSigned(int _grade);
+};
+
+std::ostream& operator<<(std::ostream &out, const Form &form);
+
+#endif
